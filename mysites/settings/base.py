f4f7201c4e9668bd54cc6cf79920be17cc0f1e01
byack <18788748257@163.com>
2019-07-30 19:29:38 +0800

    添加static_root设置，用于收集静态文件

--- mysites/settings/base.py
+++ mysites/settings/base.py
@@ -106,6 +106,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_collected')
 # 指定静态文件所放的位置
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
